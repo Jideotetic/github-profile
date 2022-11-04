a7b7546d8364c4b2114a8a8f7f85de865d9723cd
Jideotetic <jideotetic@gmail.com>
2022-11-04 22:52:08 +0100

    updated

--- src/pages/ErrorBoundaryTest.js
+++ src/pages/ErrorBoundaryTest.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom"
+import { redirect } from "react-router-dom";
 
 
 export default function ErrorBoundaryTest() {
     return (
         <>
-            <h2>Jide</h1>
+            <h1>Jide</h1>
+
+            {redirect("/home")}
             
         </>
     )
